3df4f32d0ea77ca0bc673010dfde79587390fce8
mario robayo <marioferyhwh@gmail.com>
2020-04-03 18:44:26 -0500

    final clase 3.1

--- models/country.js
+++ models/country.js
@@ -1,7 +1,23 @@
+ /**
+  * para solicitar los paises direccion
+  * peric.github.io/GetCountries/
+  */
 const mogoose = require('mongoose'),
       Schema = mogoose.Schema,
       CountryShema = new Schema({
+        code:{
+          type:String,
+          required:[true,'mensage de code'],
+          unique:true
+        },
+        name:{
+          type:String,
+          required:[true,'nombre requerido'],
+          unique:true
+        }
+
 
       })
 
       module.exports = mogoose.models('Country',CountryShema)
+
--- models/marathon.js
+++ models/marathon.js
@@ -2,6 +2,39 @@ const mongoose = require('mongoose'),
       Schema = mongoose.Schema,
       MarathoSchema = new Schema({
         /** */
+        name:{
+          type:String,
+          require :[true,'el nombre de requeridos']
+        },
+        country:{
+          type:Schema.Types.ObjectId,
+          ref:'Country',
+          require :[true,'el nombre de requeridos']
+        },
+        location:{
+          type:String,
+          require :[true,'el nombre de requeridos']
+        },
+        altitud:Number,
+        race_web:{
+          type:String,
+          lowercase:true
+        },
+        race_logo :String,
+        race_since :String,
+        race_day:String,
+        race_date:Date,
+        race_type:{
+          type:String,
+          enum:['City','Trail'],
+          default:'City'
+        },
+        distance :{
+          type:Number,
+          min:42.195,
+          default:42.195
+        },
+        resumen:String
       })
 
 
