f70f35ab1ee8a30be45b3d76216acf012624eec7
mario robayo <marioferyhwh@gmail.com>
2020-04-03 17:07:54 -0500

    fin clse 2.4

---
+++ config.js
@@ -0,0 +1,7 @@
+const c = Console.log
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.PORT = process.env.PORT || 3000
+
+if(process.env.NODE_ENV === 'dev'){
+    process.env.URL_DB = 'mongodb://localhost:27017/edmaratones'
+}
