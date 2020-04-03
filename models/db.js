e6285653d344a0bfec6b69a2383dff0f381e3225
mario robayo <marioferyhwh@gmail.com>
2020-04-03 18:00:59 -0500

    final clase 2.6

---
+++ models/db.js
@@ -0,0 +1,18 @@
+const c = console.log,
+      mongoose = require ('mongoose'),
+      config = require ('../config')
+
+class Database {
+  constructor(){
+    this.connect()
+  }
+  connect(){
+    mongoose.connect(process.env.URL_DB,{ useNewUrlParser: true } )
+    .then (()=> c(`Conexion exitosa a la base de datos :${rocess.env.URL_DB}`))
+    .catch(err => c(`Errro concexion DB :${err}`))
+
+  }
+}
+module.exports = new Database()
+
+      
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const c = console.log ,
       app = require('./app')
+      db = require('./models/db')
 
       app.listen(app.get('port'),()=> c(`Iniciado servidor por el puerto ${app.get('port')}`))
