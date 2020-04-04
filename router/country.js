9785ae9b6fbe418f301f0aff75b686c8b97ec2e7
mario robayo <marioferyhwh@gmail.com>
2020-04-03 19:49:59 -0500

    final  clase 3.4

--- router/country.js
+++ router/country.js
@@ -1,10 +1,38 @@
-const Country = require('../models/country')
+const Country = require('../models/country'),
+fs=require('fs')
 
 const PostCountries = async(req,res) =>{
   //await res.status(200).send({message: })
+   const dd = fs.readFileSync('./models/countries.json','utf-8')
+  //Console.log (dd)
+  const countries = JSON.parse(dd)
+  await Country.insertMany(countries,(err,data)=>{
+    if (err){
+      res.status(500).send({
+        message:`error Inteno del servidor ${err}`
+      })      
+      //return
+    }else{
+    res.status(200).send({data})
+    }
+  })
 }
 const GetCountries = async(req,res) =>{
   //await res.status(200).send({message: })
+  await Country.find({}).exec((err,data)=>{
+    if (err){
+      res.status(500).send({
+        message:`error Inteno del servidor ${err}`
+      })            
+    }
+    if (data){
+    res.status(200).send({data})
+    }
+    else{
+      res.status(404).send({message:`no se encontro informacion`})
+
+    }
+  })
 }
 
 module.exports = {
--- router/marathon.js
+++ router/marathon.js
@@ -1,29 +1,45 @@
 const Marathon = require('../models/marathon')
 
+const apiResponse = ((req,res,err,data)=>{
+  if (err){
+    res.status(500).send({
+      message:`error Inteno del servidor ${err}`
+    })            
+  }
+  if (data){
+  res.status(200).send({data})
+  }
+  else{
+    res.status(404).send({message:`no hay datos con esoso parametros`})
+
+  }
+})
 
 const getMarathons = async(req,res) =>{
-  //await res.status(200).send({message: })
-},
-postMarathons = async(req,res) =>{
-  //await res.status(200).send({message: })
-},
-getMarathon = async(req,res) =>{
-  //await res.status(200).send({message: })
+  await Marathon.find({}).sort({'_id':-1}).exec((err,data)=>apiResponse(req,res,err,data))
 },
 postMarathon = async(req,res) =>{
-  //await res.status(200).send({message: })
+  console.log(req.body)
+  let marathon = new Marathon(req.body)
+  await Marathon.save((err,data)=>apiResponse(req,res,err,data))
+},
+getMarathon = async(req,res) =>{
+  await Marathon
+  .findById(req.params.id)
+  .exec((err,data)=>apiResponse(req,res,err,data))
 },
 putMarathon = async(req,res) =>{
-  //await res.status(200).send({message: })
+  await Marathon
+  .findByIdAndUpdate(req.params.id,req.params.body,{new:true},(err,data)=>apiResponse(req,res,err,data))
 },
 deleteMarathon = async(req,res) =>{
-  //await res.status(200).send({message: })
+  await Marathon
+  .findByIdAndRemove(req.params.id,(err,data)=>apiResponse(req,res,err,data))
 }
 module.exports ={
   getMarathons,
   getMarathon,
   postMarathon,
-  postMarathons,
   putMarathon,
   deleteMarathon
 }
